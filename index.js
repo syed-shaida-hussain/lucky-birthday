a04c86c0720104844d1525edd04b9e35e0a4bf77
syed shaida <syedshaida295@gmail.com>
2021-08-24 12:48:28 +0530

    added messages and output.

--- index.js
+++ index.js
@@ -1,30 +1,31 @@
 const dateOfBirth = document.querySelector("#date-of-birth");
 const luckyNumber = document.querySelector("#lucky-number");
 const checkButton = document.querySelector("#check-button");
+const outputBox = document.querySelector("#output-box");
 
 function checkBirthDateIsLucky() {
     const dob = dateOfBirth.value;
     const sum = calculateSum(dob);
-    console.log(sum);
-    console.log(compareValues(sum,luckyNumber.value));
+    console.log(compareValues(sum,luckyNumber.value))
 }
 
-function compareValues(sum, luckyNumber) {
+function compareValues(sum,luckyNumber) {
     if (sum % luckyNumber === 0) {
-        console.log("lucky");
+        outputBox.innerText = "your birthday is lucky 🥳";
     }
     else{
-        console.log("unlucky");
+        outputBox.innerText= "your birthday is unlucky 😢. Work hard and create your own luck ";
     }
+}
 
-    function calculateSum(dob) {
-        dob = dob.replaceAll("-","");
-        let sum = 0;
-        for (let i = 0; i < dob.length; i++) {
-            sum = sum + Number(dob.charAt(i));
-        }
-        return sum;
-
+function calculateSum(dob) {
+    dob = dob.replaceAll("-","");
+    let sum = 0;
+    for (let i = 0; i < dob.length; i++) {
+        sum = sum + Number(dob.charAt(i));
     }
+    return sum;
 
-    checkButton.addEventListener("click", checkBirthDateIsLucky);
+}
+
+checkButton.addEventListener("click",checkBirthDateIsLucky);
